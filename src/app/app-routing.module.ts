7141ce0d89d5727f743f359bd5b4bb58ea6e718d
Amit Patange <amitpatange88@gmail.com>
2020-07-29 20:47:39 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SeedComponent } from './seed/seed.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'Seed', component: SeedComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
